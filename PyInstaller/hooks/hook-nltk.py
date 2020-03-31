1318e5b1656c2fe801fc7ebb757701e7564f6572
James Blackburn <blackburnfjames@gmail.com>
2020-03-31 19:36:55 +0100

    Fixes nltk_data errors

--- PyInstaller/hooks/hook-nltk.py
+++ PyInstaller/hooks/hook-nltk.py
@@ -15,11 +15,12 @@ import nltk
 from PyInstaller.utils.hooks import collect_data_files
 
 # add datas for nltk
-datas = collect_data_files('nltk', False)
+# datas = collect_data_files('nltk', False)
+datas = []
 
 # loop through the data directories and add them
-for p in nltk.data.path:
-    datas.append((p, "nltk_data"))
+# for p in nltk.data.path:
+#     datas.append((p, "nltk_data"))
 
 # nltk.chunk.named_entity should be included
 hiddenimports = ["nltk.chunk.named_entity"]
